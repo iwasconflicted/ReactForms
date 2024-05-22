3d0e7051308e39e1ea903c4f064112bbd52a6dd8
Saroeut Sim <saroeuts123@gmail.com>
2024-05-21 18:35:05 -0700

    first commit

---
+++ react_formsl/src/App.tsx
@@ -0,0 +1,23 @@
+import FormUseState from "./components/Forms/FormUseState"
+
+
+const App = () => {
+  return (
+
+
+
+    <>
+     
+      <FormUseState/>
+
+
+
+    </>
+
+
+
+
+  )
+}
+
+export default App
